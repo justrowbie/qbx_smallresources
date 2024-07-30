084fbee5d24991b831ca2083ee3982b06483529f
justrowbie <rowbieplaying@gmail.com>
2024-07-31 01:19:02 +0300

    update hud component

--- qbx_hudcomponents/config.lua
+++ qbx_hudcomponents/config.lua
@@ -1,10 +1,10 @@
 return {
     disable = {
         -- https://docs.fivem.net/natives/?_0x6806C51AD12B83B8
-        hudComponents = {1, 2, 3, 4, 7, 9, 13, 14, 19, 20, 21, 22},
+        hudComponents = { 1, 2, 3, 4, 7, 9, 13, 19, 20, 21, 22 },
 
         -- https://docs.fivem.net/docs/game-references/controls/
-        controls = {37},
+        controls = { 16, 17, 26, 36, 37, 80, 81, 82, 83, 84, 85, 99, 100, 115, 116, 140, 141, 257, 261, 262 },
 
         -- False disables ammo display
         displayAmmo = true,
